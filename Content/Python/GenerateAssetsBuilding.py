888474c94c64518081dd13667607fdd27c5c0404
PQNDAAA <gayvallet.dylan@gmail.com>
2023-03-16 10:08:45 +0100

    Build C++

--- Content/Python/GenerateAssetsBuilding.py
+++ Content/Python/GenerateAssetsBuilding.py
@@ -5,12 +5,15 @@ def GenerateBuilding(asset_package, asset_name):
   
     parent = unreal.EditorAssetLibrary.load_blueprint_class("/Game/BP_Items/Items/BP_ItemBase")
     factory = unreal.BlueprintFactory()
+    factory_1 = unreal.DataAssetFactory()
     factory.set_editor_property("parent_class",parent)
+    factory_1.context_class()
 
+    unreal.AssetToolsHelpers.get_asset_tools().create_asset("DA_"+asset_name,asset_package,None,factory_1)
     unreal.AssetToolsHelpers.get_asset_tools().create_asset(asset_name,asset_package,None,factory)
     
 def Generate():
-     GenerateBuilding("/Game/Assets","teda")
+     GenerateBuilding("/Game/Assets","teda",)
 
 
 def printBlueprint():
