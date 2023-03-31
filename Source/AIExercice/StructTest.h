bb98aa051dda3562a5670efb4dd7ecbe21a7af06
PQNDAAA <gayvallet.dylan@gmail.com>
2023-03-31 12:06:43 +0200

    Update Test C++

---
+++ Source/AIExercice/StructTest.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "Engine/DataTable.h"
+#include "enu.h"
+#include "Microsoft/AllowMicrosoftPlatformTypes.h"
+#include "StructTest.generated.h"
+
+USTRUCT(BlueprintType)
+struct FStructTest : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	
+	// Propriétés de la DataRow
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyDataRow")
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyDataRow")
+	Eenu myenum;
+	
+	// Constructeur par défaut
+	FStructTest() {}
+};
---
+++ Source/AIExercice/enu.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "enu.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class Eenu : uint8
+{
+	Sword,
+	Shield,
+	Potion
+};
