983fc18c3d600c1c45f045ecd02b75a22bfe1878
PQNDAAA <gayvallet.dylan@gmail.com>
2023-05-05 02:05:23 +0200

    Folder organization

--- Content/Python/GenerateAssetsBuilding.py
+++ Content/Python/GenerateAssetsBuilding.py
@@ -3,7 +3,7 @@ import unreal
 
 def GenerateBuilding(asset_package, asset_name):
   
-    parent = unreal.EditorAssetLibrary.load_blueprint_class("/Game/Building/StorageBuilding/BP_StorageBuildingBase")
+    parent = unreal.EditorAssetLibrary.load_blueprint_class("/Game/Building/WorkBuilding/BP_WorkBuildingBase")
     factory = unreal.BlueprintFactory()
     factory.set_editor_property("parent_class",parent)
     unreal.AssetToolsHelpers.get_asset_tools().create_asset(asset_name,asset_package,None,factory)
@@ -24,29 +24,14 @@ def printBlueprint():
         print (bcd.get_class())
         print (bcd.get_class().get_name())
 
-def editVariables(pathbuilding):
-
-    # get the generated class of the Blueprint (note the _C)
-    bp_gc = unreal.load_object(None, pathbuilding)
-    # get the Class Default Object (CDO) of the generated class
-    bp_cdo = unreal.get_default_object(bp_gc)
-
-    return bp_cdo.get_editor_property("villagersMax")
-        # set the default property values
-    #bp_cdo.set_editor_property("villagersMax", 1)
-
 
 def SetVillagersMax(pathbuilding,villagers):
-     # get the generated class of the Blueprint (note the _C)
     bp_gc = unreal.load_object(None, pathbuilding)
-    # get the Class Default Object (CDO) of the generated class
     bp_cdo = unreal.get_default_object(bp_gc)
     bp_cdo.set_editor_property("villagersMax", villagers)
 
 def SetStaticMesh(pathbuilding,sm):
-    # get the generated class of the Blueprint (note the _C)
     bp_gc = unreal.load_object(None, pathbuilding)
-    # get the Class Default Object (CDO) of the generated class
     bp_cdo = unreal.get_default_object(bp_gc)
     bp_cdo.set_editor_property("Static_Mesh_Reference", sm)
 
@@ -86,10 +71,7 @@ def SetUnlockingLevel(path, unlockingLevel):
     bp_cdo.set_editor_property("unlockingLevel", unlockingLevel)
 
 def GetStaticMesh(pathbuilding):
-
-# get the generated class of the Blueprint (note the _C)
     bp_gc = unreal.load_object(None, pathbuilding)
-    # get the Class Default Object (CDO) of the generated class
     bp_cdo = unreal.get_default_object(bp_gc)
     return bp_cdo.get_editor_property("Static_Mesh_Reference")
 
