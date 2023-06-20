34cd9996da72dc59acaf9bdb12d9f7bdd20b4621
PQNDAAA <gayvallet.dylan@gmail.com>
2023-06-21 01:58:00 +0200

    Some C++ functions added for construction mode
    - A function to move the actor in real-time is added
    - A function to calculate a camera raycast is added
    These c++ functions are repeated every 0.5 seconds in blueprint to refresh if the player's location has changed

---
+++ Source/AIExercice/CharacterPawn.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterPawn.h"
+
+#include <algorithm>
+
+// Sets default values
+ACharacterPawn::ACharacterPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACharacterPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PC = Cast<APlayerController>(GetController());
+
+	if(IsValid(PC))
+	{
+		PC->bShowMouseCursor = true;
+		PC->bEnableMouseOverEvents = true;
+	}
+	
+}
+
+// Called every frame
+void ACharacterPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACharacterPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+FVector ACharacterPawn::ForwardRaycastProto(UCameraComponent* Camera)
+{
+	FVector CameraWorldLocation = Camera->GetComponentLocation();
+	FVector CameraForwardVector = Camera->GetForwardVector();
+	FVector RaycastEndLocation = CameraForwardVector * 100000.0f;
+
+	FVector RaycastFinalLocation = CameraWorldLocation + RaycastEndLocation;
+
+	//Raycast Start = CameraWorldLocation
+	//Raycast End = RaycastEndLocation 
+
+	return RaycastEndLocation;
+}
+
+void ACharacterPawn::MoveActorProto(AActor* actor, FVector loc)
+{
+	actor->SetActorLocation(loc);
+}
+
+
+
---
+++ Source/AIExercice/CharacterPawn.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Pawn.h"
+#include "CharacterPawn.generated.h"
+
+UCLASS()
+class AIEXERCICE_API ACharacterPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACharacterPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable, Category="ConstructionMode")
+	FVector ForwardRaycastProto(UCameraComponent* Camera);
+
+	UFUNCTION(BlueprintCallable, Category="ConstructionMode")
+	void MoveActorProto(AActor* actor, FVector loc);
+
+};
